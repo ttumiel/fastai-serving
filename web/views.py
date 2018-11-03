542aaae8c96e73db36d6dfdb13240dcdb6c86bb2
Thomas Tumiel <ttumiel@gmail.com>
2018-11-03 23:55:36 +0200

    separate img name and prediction

--- web/views.py
+++ web/views.py
@@ -8,7 +8,8 @@ def index(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            pred, prob = evaluate(request.FILES['image'])
+            img_name = save_image(request.FILES['image'])
+            pred, prob = predict(img_name)
             return render(request, 'web/results.html', {'pred': pred, 'prob': prob})
     form = UploadFileForm()
     return render(request, 'web/index.html', {'form': form})
