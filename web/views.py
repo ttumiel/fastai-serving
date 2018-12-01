c6af0ec23902b1d98ffe8ee3777316ad3e869dcf
Thomas Tumiel <ttumiel@gmail.com>
2018-12-01 15:20:53 +0200

    namespace datamodels

--- web/views.py
+++ web/views.py
@@ -2,18 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
 from .forms import UploadFileForm
-import datamodels
+import datamodels as D
 
 def index(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            img_name = save_image(request.FILES['image'])
-            pred, prob = predict(img_name)
+            img_name = D.save_image(request.FILES['image'])
+            pred, prob = D.predict(img_name)
             return render(request, 'web/results.html', {'pred': pred, 'prob': prob})
     form = UploadFileForm()
     return render(request, 'web/index.html', {'form': form})
 
-# Should use separate view
+# Should use separate view and an ajax response
 def results(request):
     return render(request, 'web/results.html', {'result': result})
