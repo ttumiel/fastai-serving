714134d0ee3f0460e56312eb68b7388934fbaf9f
Thomas Tumiel <ttumiel@gmail.com>
2019-02-15 19:30:52 +0200

    clean imports, add docstrings

--- datamodels/utils.py
+++ datamodels/utils.py
@@ -2,24 +2,28 @@
 # package this file with the models folder instead for global
 # access my multiple apps. Also, this just makes more sense
 
-
-from fastai import *
-from fastai.vision import *
+from fastai.vision import create_cnn, ImageDataBunch, open_image, get_transforms, models
+from fastai.metrics import accuracy
 import torch.nn.functional as F
 
 imageNum = 0
 
-data = ImageDataBunch.from_folder("datamodels/", ds_tfms=get_transforms(), test='test', size=224)
+data = ImageDataBunch.from_folder("datamodels/", ds_tfms=get_transforms(),
+                                    test='test', size=224, bs=1)
 learn = create_cnn(data, models.resnet34, metrics=accuracy)
 learn.load("model", device="cpu")
 learn.precompute=False
-learn.data.train_dl.batch_size = 1
 
 def save_image(image):
     """
-    Save an uploaded image for processing
+    Save an uploaded image for processing.
 
-    return image name
+    Args:
+        image: UploadedFile
+            The image that is to be classified.
+
+    Returns:
+        str: image name
     """
     global imageNum
     imageNum+=1
@@ -33,7 +37,12 @@ def predict(imagePath):
     """
     Evaluate an image using a trained pytorch model
 
-    returns (predicted_category, probability)
+    Args:
+        imagePath: str
+            The path to the image that is to be classified.
+
+    Returns:
+        tuple: (str: category, float: probability)
     """
     global learn
 
