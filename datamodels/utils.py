c4ca4b28667329f14ef7ea8c389a382622027452
Thomas Tumiel <ttumiel@gmail.com>
2019-02-15 19:18:43 +0200

    update model utils for fastai update

--- datamodels/utils.py
+++ datamodels/utils.py
@@ -1,5 +1,5 @@
 # Utility file that will connect to pytorch to evaluate models
-# package this file with the models folder instead for global 
+# package this file with the models folder instead for global
 # access my multiple apps. Also, this just makes more sense
 
 
@@ -9,15 +9,16 @@ import torch.nn.functional as F
 
 imageNum = 0
 
-data = ImageDataBunch.from_folder("datamodels/", ds_tfms=get_transforms(), test='test', tfms=imagenet_norm, size=224)
-learn = ConvLearner(data, models.resnet34, metrics=accuracy)
-learn.load(f"dog_cat_model_cpu")
+data = ImageDataBunch.from_folder("datamodels/", ds_tfms=get_transforms(), test='test', size=224)
+learn = create_cnn(data, models.resnet34, metrics=accuracy)
+learn.load("model", device="cpu")
 learn.precompute=False
+learn.data.train_dl.batch_size = 1
 
 def save_image(image):
     """
     Save an uploaded image for processing
-    
+
     return image name
     """
     global imageNum
@@ -32,10 +33,10 @@ def predict(imagePath):
     """
     Evaluate an image using a trained pytorch model
 
-    returns (predicted category, probability)
+    returns (predicted_category, probability)
     """
     global learn
 
     im = open_image(imagePath)
-    preds = im.predict(learn)
-    return (learn.data.classes[np.argmax(preds)], np.max(F.softmax(preds, dim=0).numpy()))
+    cat, argm, probs = learn.predict(im)
+    return cat, torch.max(probs).item()
